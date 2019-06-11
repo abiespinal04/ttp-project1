829525412067e004799487a18ff21ee279865269
pawelmisiak <thepawelmisiak@gmail.com>
2019-06-10 21:02:33 -0400

    added js links to bootstrap to the index.html and fixed the issue with the nav

--- FrontEnd/src/components/NavBar.js
+++ FrontEnd/src/components/NavBar.js
@@ -1,30 +1,39 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
+const NavBar = props => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link class="navbar-brand" to="/">
+        Home
+      </Link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item ">
+            <Link class="nav-link" to="/campusListing">
+              Campuses<span class="sr-only">(current)</span>
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="students">
+              Students
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-
-const NavBar = (props) => {
-    return ( 
-
-      
-           <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <Link class="navbar-brand" to="/">Home</Link>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <Link class="nav-link" to="/campusListing">Campuses<span class="sr-only">(current)</span></Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="students" >Students</Link>
-      </li>
-    </ul>
-  </div>
-</nav>
-       
-     );
-}
- 
 export default NavBar;
