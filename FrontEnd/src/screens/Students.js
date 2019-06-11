4bd67db726b0eb6bdb93f708d781380fecabd484
pawelmisiak <thepawelmisiak@gmail.com>
2019-06-10 22:26:40 -0400

    changed the general look and button

--- FrontEnd/src/screens/Students.js
+++ FrontEnd/src/screens/Students.js
@@ -1,106 +1,108 @@
-import React, { Component } from 'react';
-import StudentCard from '../components/StudentCard';
-import AddStudentButton from '../components/AddStudentButton'
-import * as actions from '../store/actions'
-import {connect} from 'react-redux'
-import Axios from 'axios'
-
+import React, { Component } from "react";
+import StudentCard from "../components/StudentCard";
+import AddStudentButton from "../components/AddStudentButton";
+import * as actions from "../store/actions";
+import "../CSS/Students.css";
+import { connect } from "react-redux";
+import Axios from "axios";
 
 class Student extends Component {
-    state = { 
-        studentList :[ 
+  state = {
+    studentList: []
+  };
 
-        ]
-     }
+  // async componentDidMount(){
+  //     const {data} = await Axios.get()
+  //     this.setState({studentList:data});
+  //  }
 
-    // async componentDidMount(){
-    //     const {data} = await Axios.get()
-    //     this.setState({studentList:data});
+  componentDidMount() {
+    console.log("Inside componentDidMount", this.props.StudentsList);
+    this.setState({ studentList: this.props.StudentsList.users });
+  }
+
+  handleNewList = newList => {
+    this.setState({ studentList: newList });
+  };
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (
+      this.state.studentList != nextProps.DeletedStudList.users &&
+      nextState.studentList
+    );
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("New StudentList", this.props.StudentsList);
+    // if(prevProps.DeletedStudList.users !== this.props.DeletedStudList.users){
+    //    this.handleNewList(this.props.DeletedStudList.users)
+    // }
+    // if(prevProps.DeletedStudList.users !== this.props.DeletedStudList.users){
+    //     this.handleNewList(this.props.DeletedStudList.users)
     //  }
 
-     componentDidMount(){
-        console.log("Inside componentDidMount",this.props.StudentsList)
-        this.setState({studentList:this.props.StudentsList.users})
-     }
-
-     handleNewList = (newList) => {
-         this.setState({studentList:newList})
-     }
-
-     shouldComponentUpdate(nextProps, nextState) {
-        return this.state.studentList != nextProps.DeletedStudList.users && nextState.studentList
-      }
-
-      componentDidUpdate(prevProps,prevState) {
-        console.log("New StudentList", this.props.StudentsList)
-        // if(prevProps.DeletedStudList.users !== this.props.DeletedStudList.users){
-        //    this.handleNewList(this.props.DeletedStudList.users)
-        // }
-        // if(prevProps.DeletedStudList.users !== this.props.DeletedStudList.users){
-        //     this.handleNewList(this.props.DeletedStudList.users)
-        //  }
-
-           if(prevProps.StudentsList.users !== this.props.StudentsList.users){
-             
-            this.setState({studentList:this.props.StudentsList})
-           
-        }
+    if (prevProps.StudentsList.users !== this.props.StudentsList.users) {
+      this.setState({ studentList: this.props.StudentsList });
     }
-    handleStudentList = () => {
-        
-        if(this.props.StudentsList === 0){
-            return <p>No Students in the database</p>
-        }else{
-        return(
-                this.state.studentList.map( (student,index) =>  
-                    <div style={{
-                     margin:20,
-                     borderStyle:'solid', 
-                     borderBottomWidth:3,
-                     borderColor:'black'}}
-                     >
-                         <StudentCard 
-                            index={index}
-                         studentName={student}
-                         />
-                    </div>
-                )
-        )
-        }
-    }
-
-    handleState = () => {
-        console.log("Inside HANDLESTATE",this.props.DeletedStudList.users)
-        this.setState({studentList:this.props.DeletedStudList.users})
-    }
-    render() { 
-        return ( 
-            <React.Fragment>
-            <div style={{display:'flex', flexDirection:'row', width:'100%'}}>
-             <div style={{display:'flex', marginTop:10, marginLeft:60,marginRight:'71%',}}>
-        <h1 style={{fontFamily:'Impact'}}>
-          Students
-        </h1>
+  }
+  handleStudentList = () => {
+    if (this.props.StudentsList === 0) {
+      return <p>No Students in the database</p>;
+    } else {
+      return this.state.studentList.map((student, index) => (
+        <div
+          style={{
+            margin: 20,
+            borderStyle: "solid",
+            borderBottomWidth: 3,
+            borderColor: "black"
+          }}
+        >
+          <StudentCard index={index} studentName={student} />
         </div>
-        <div style={{marginTop:10}}>
-            <AddStudentButton/>
+      ));
+    }
+  };
+
+  handleState = () => {
+    console.log("Inside HANDLESTATE", this.props.DeletedStudList.users);
+    this.setState({ studentList: this.props.DeletedStudList.users });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <div id="studentBody">
+          <div id="titlePlusAddStudent">
+            <div
+              style={{
+                display: "flex",
+                marginTop: 10,
+                marginLeft: 60,
+                marginRight: "71%"
+              }}
+            >
+              <h1>Students</h1>
             </div>
-    </div>
-    <div>
-    {this.handleStudentList()} 
-    </div>
-    {/* <button onClick ={this.handleState}> updateState</button> */}
-    </React.Fragment>
-     );
-    }
+            <div id="addButton">
+              <AddStudentButton />
+            </div>
+          </div>
+          <div>{this.handleStudentList()}</div>
+          {/* <button onClick ={this.handleState}> updateState</button> */}
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
+  return {
+    StudentsList: state.StudentsList,
+    DeletedStudList: state.DeletedStudent
+  };
+};
 
-    return{
-        StudentsList : state.StudentsList,
-        DeletedStudList: state.DeletedStudent
-    }
-}
- 
-export default connect(mapStateToProps,actions)(Student); 
+export default connect(
+  mapStateToProps,
+  actions
+)(Student);
