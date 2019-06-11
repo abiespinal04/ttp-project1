965485a82be02fb882ad5fb56f94a0d00e0dc608
pawelmisiak <thepawelmisiak@gmail.com>
2019-06-11 14:56:44 -0400

    saving the change

--- FrontEnd/src/components/Delete.js
+++ FrontEnd/src/components/Delete.js
@@ -17,26 +17,4 @@ const Delete = props => {
   );
 };
 
-<<<<<<< HEAD
-
-const Delete = (props) => {
-
-   const handleDeleteStudent = () => {
-        props.handleDelete()
-    }
-    return ( 
-        <div>
-            <button 
-            className="DeleteContainer"
-            onClick={handleDeleteStudent}
-            >
-                Delete
-            </button>
-        </div>
-     );
-}
- 
 export default Delete;
-=======
-export default Delete;
->>>>>>> student
