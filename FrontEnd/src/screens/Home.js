68e9d6f0b4dd5241323bc12cfdfeafbe7af5dcd4
Tarik <lightspeedkey@yahoo.com>
2019-06-14 16:04:08 -0400

    background layer

--- FrontEnd/src/screens/Home.js
+++ FrontEnd/src/screens/Home.js
@@ -3,12 +3,13 @@ import "../CSS/Home.css";
 class Home extends Component {
   state = {};
   render() {
+    
     return (
-      <img
-        src="https://www.dreadcentral.com/wp-content/uploads/2016/11/Generic-Scary-School.jpeg"
-        className="homePicture"
-        alt="pic"
-      />
+      <body id = "homeb">
+
+        <h1>Lorem ipsum pharetra euismod conubia leo fusce aliquet quam, molestie praesent rhoncus tempor sit eu nibh purus, egestas urna aliquam eu fringilla phasellus duis ad donec dapibus condimentum litora.</h1>
+
+      </body>
     );
   }
 }
