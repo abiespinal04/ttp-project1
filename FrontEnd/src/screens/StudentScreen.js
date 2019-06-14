2f948cd774e823210782afafd47c5555e62d0656
Abimael Espinal <abimaelespinal@gmail.com>
2019-06-14 13:35:13 -0400

    started styling student screen

--- FrontEnd/src/screens/StudentScreen.js
+++ FrontEnd/src/screens/StudentScreen.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
-
+import '../CSS/StudentScreen.css'
 
 class StudentScreen extends Component {
     state = {  }
     render() { 
         return ( 
 
-            <div>
-            <div>
-                <p>{this.props.location.student.firstName}</p>
-                <p>{this.props.location.student.lastName}</p>
+            <div className="StudentContainer">
+            <div className="StudentInfo">
+                <h1>{this.props.location.student.firstName}</h1>
+                <h4>{this.props.location.student.lastName}</h4>
                 <p>{this.props.location.student.imageURL}</p>
                 <p>{this.props.location.student.EMPID}</p>
                 <p>{this.props.location.student.description}</p>
             </div>
             <div>
                 <Link to='students'>
-               <button>GO BACK</button> 
+                <button type="button" class="btn btn-dark">GO BACK</button>
+
                </Link>
             </div>
             </div>
